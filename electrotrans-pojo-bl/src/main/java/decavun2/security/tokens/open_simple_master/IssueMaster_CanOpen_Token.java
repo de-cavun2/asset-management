4559d9e689f8ba6e848b3aadc234c4c9ed8f6b9e
Markiian Tsalyk <m.tsalyk@ucu.edu.ua>
2023-01-20 23:27:02 +0200

    Update tokens

---
+++ electrotrans-pojo-bl/src/main/java/decavun2/security/tokens/open_simple_master/IssueMaster_CanOpen_Token.java
@@ -0,0 +1,19 @@
+package decavun2.security.tokens.open_simple_master;
+
+import static java.lang.String.format;
+
+import ua.com.fielden.platform.reflection.TitlesDescsGetter;
+import ua.com.fielden.platform.security.tokens.Template;
+import decavun2.change.Issue;
+import decavun2.security.tokens.UsersAndPersonnelModuleToken;
+
+/**
+ * A security token for entity {@link Issue} to guard Open.
+ *
+ * @author Developers
+ *
+ */
+public class IssueMaster_CanOpen_Token extends UsersAndPersonnelModuleToken {
+    public final static String TITLE = format(Template.MASTER_OPEN.forTitle(), Issue.ENTITY_TITLE + " Master");
+    public final static String DESC = format(Template.MASTER_OPEN.forDesc(), Issue.ENTITY_TITLE);
+}
---
+++ electrotrans-pojo-bl/src/main/java/decavun2/security/tokens/persistent/Issue_CanDelete_Token.java
@@ -0,0 +1,19 @@
+package decavun2.security.tokens.persistent;
+
+import static java.lang.String.format;
+
+import ua.com.fielden.platform.reflection.TitlesDescsGetter;
+import ua.com.fielden.platform.security.tokens.Template;
+import decavun2.change.Issue;
+import decavun2.security.tokens.UsersAndPersonnelModuleToken;
+
+/**
+ * A security token for entity {@link Issue} to guard Delete.
+ *
+ * @author Developers
+ *
+ */
+public class Issue_CanDelete_Token extends UsersAndPersonnelModuleToken {
+    public final static String TITLE = format(Template.DELETE.forTitle(), Issue.ENTITY_TITLE);
+    public final static String DESC = format(Template.DELETE.forDesc(), Issue.ENTITY_TITLE);
+}
