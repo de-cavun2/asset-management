24354fd2f326f386d01bb6278106bcb4f7888973
Stefan <impr0digg@gmail.com>
2023-01-20 19:56:11 +0200

    #15 Started implementing integrity constraints

--- electrotrans-pojo-bl/src/main/java/decavun2/config/ApplicationDomain.java
+++ electrotrans-pojo-bl/src/main/java/decavun2/config/ApplicationDomain.java
@@ -10,6 +10,14 @@ import decavun2.personnel.Person;
 import ua.com.fielden.platform.basic.config.IApplicationDomainProvider;
 import ua.com.fielden.platform.domain.PlatformDomainTypes;
 import ua.com.fielden.platform.entity.AbstractEntity;
+import decavun2.change.Report;
+import decavun2.change.Issue;
+import decavun2.change.Change;
+import decavun2.change.ui_actions.OpenChangeMasterAction;
+import decavun2.change.master.menu.actions.ChangeMaster_OpenMain_MenuItem;
+import decavun2.change.master.menu.actions.ChangeMaster_OpenIssue_MenuItem;
+import decavun2.change.master.menu.actions.ChangeMaster_OpenReport_MenuItem;
+import decavun2.analysis.Statistics;
 
 /**
  * A class to register domain entities.
@@ -18,26 +26,34 @@ import ua.com.fielden.platform.entity.AbstractEntity;
  */
 public class ApplicationDomain implements IApplicationDomainProvider {
 
-    private static final Set<Class<? extends AbstractEntity<?>>> entityTypes = new LinkedHashSet<>();
-    private static final Set<Class<? extends AbstractEntity<?>>> domainTypes = new LinkedHashSet<>();
+	private static final Set<Class<? extends AbstractEntity<?>>> entityTypes = new LinkedHashSet<>();
+	private static final Set<Class<? extends AbstractEntity<?>>> domainTypes = new LinkedHashSet<>();
 
-    static {
-        entityTypes.addAll(PlatformDomainTypes.types);
-        add(Person.class);
-    }
+	static {
+		entityTypes.addAll(PlatformDomainTypes.types);
+		add(Person.class);
+		add(Report.class);
+		add(Issue.class);
+		add(Change.class);
+		add(OpenChangeMasterAction.class);
+		add(ChangeMaster_OpenMain_MenuItem.class);
+		add(ChangeMaster_OpenIssue_MenuItem.class);
+		add(ChangeMaster_OpenReport_MenuItem.class);
+		add(Statistics.class);
+	}
 
-    private static void add(final Class<? extends AbstractEntity<?>> domainType) {
-        entityTypes.add(domainType);
-        domainTypes.add(domainType);
-    }
+	private static void add(final Class<? extends AbstractEntity<?>> domainType) {
+		entityTypes.add(domainType);
+		domainTypes.add(domainType);
+	}
 
-    @Override
-    public List<Class<? extends AbstractEntity<?>>> entityTypes() {
-        return Collections.unmodifiableList(entityTypes.stream().collect(Collectors.toList()));
-    }
+	@Override
+	public List<Class<? extends AbstractEntity<?>>> entityTypes() {
+		return Collections.unmodifiableList(entityTypes.stream().collect(Collectors.toList()));
+	}
 
-    public List<Class<? extends AbstractEntity<?>>> domainTypes() {
-        return Collections.unmodifiableList(domainTypes.stream().collect(Collectors.toList()));
-    }
+	public List<Class<? extends AbstractEntity<?>>> domainTypes() {
+		return Collections.unmodifiableList(domainTypes.stream().collect(Collectors.toList()));
+	}
 
 }
